eb3d30a51caa2aad1963b5bfc9a0e32d5ebb0cc1
PhongNgo <ngothanhphong1776@gmail.com>
2025-06-05 11:02:12 +0700

    Init react native code

--- rn_app/ios/RNExample/AppDelegate.swift
+++ rn_app/ios/RNExample/AppDelegate.swift
@@ -17,7 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
-    // FlutterManager.shared.setup()
 
     reactNativeDelegate = delegate
     reactNativeFactory = factory
