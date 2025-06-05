4c03b42ec971e6e212de3e60e8bea5d94c1cb7f4
PhongNgo <ngothanhphong1776@gmail.com>
2025-06-05 12:07:28 +0700

    Update to include Flutter iOS framework into RN

--- fluttermodule/lib/main.dart
+++ fluttermodule/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
-// Import for iOS/macOS features.
 
 Future<void> main() async {
   runApp(MaterialApp(home: WebViewExample()));
@@ -38,62 +37,3 @@ class _WebViewExampleState extends State<WebViewExample> {
     );
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
-
-// void main() => runApp(const LocalTextApp());
-
-// class LocalTextApp extends StatelessWidget {
-//   const LocalTextApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: const Text('Text Viewer')),
-//         body: const LocalTextViewer(),
-//       ),
-//     );
-//   }
-// }
-
-// class LocalTextViewer extends StatefulWidget {
-//   const LocalTextViewer({super.key});
-
-//   @override
-//   State<LocalTextViewer> createState() => _LocalTextViewerState();
-// }
-
-// class _LocalTextViewerState extends State<LocalTextViewer> {
-//   @override
-//   void initState() {
-//     WidgetsBinding.instance.addPostFrameCallback((_) {
-//       Future.delayed(const Duration(seconds: 5), () {
-//         setState(() {});
-//       });
-//     });
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     debugPrint('Building LocalTextViewer');
-//     const htmlContent = '''
-//       <!DOCTYPE html>
-//       <html>
-//       </head>
-//       <body>
-//         <h1>Local Text Display</h1>
-//         <p>This content is loaded locally in the WebView.</p>
-//         <p>You can display formatted text, code snippets, or any HTML content.</p>
-//       </body>
-//       </html>
-//     ''';
-
-//     return WebViewWidget(
-//         controller: WebViewController()
-//           ..loadHtmlString(htmlContent)
-//           ..setJavaScriptMode(JavaScriptMode.unrestricted));
-//   }
-// }
---
+++ rn_app/ios/Frameworks/StandingTall.podspec
@@ -0,0 +1,13 @@
+Pod::Spec.new do |s|
+  s.name             = 'StandingTall'
+  s.version          = '1.0.0' # Set an appropriate version
+  s.summary          = 'Flutter framework for integration with React Native'
+  s.description      = 'A custom podspec for including Flutter.framework in a React Native project.'
+  s.homepage         = 'https://flutter.dev'
+  s.license          = { :type => 'MIT', :file => 'LICENSE' } # Adjust license as needed
+  s.author           = { 'Your Name' => 'your.email@example.com' }
+  s.source           = { :path => '.' }
+  s.platform         = :ios, '13.0' # Match your project’s deployment target
+  s.vendored_frameworks = 'App.framework'
+  s.requires_arc     = true
+end
